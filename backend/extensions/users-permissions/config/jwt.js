ed20cc7dd07c7e83e5c7cdcb8ba6edd69242dca7
Q.Tran <tranlehaiquan@gmail.com>
2021-05-29 18:38:36 +0700

    Update fe folder

---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '32a3af29-5b2f-4d04-9297-4c00a2ccf376'
+};
---
+++ market-front/pages/_app.tsx
@@ -0,0 +1,22 @@
+import "nprogress/nprogress.css";
+import Ngr from "nprogress";
+import Router from "next/router";
+import { ApolloProvider } from "@apollo/client";
+
+import { useApollo } from "../lib/apolloClient";
+
+Router.events.on("routeChangeStart", () => Ngr.start());
+Router.events.on("routeChangeComplete", () => Ngr.done());
+Router.events.on("routeChangeError", () => Ngr.done());
+
+function MyApp({ Component, pageProps }) {
+  const apolloClient = useApollo(pageProps);
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
+
+export default MyApp;
